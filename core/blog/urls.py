d383d814bebed8cc296a40f60641df10cc2ee6a6
arshia-shiekh-bahayi <arshiasheikhbahayi@gmail.com>
2024-04-12 18:59:02 +0430

    added a post form and made to be editable and deleteable for its author

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from django.views.generic import TemplateView
-from django.views.generic.base import RedirectView
+from django.views.generic import *
 from .views import *
 app_name = 'blog'
 urlpatterns = [
     # path('cbv-index', views.IndexView.as_view(),name='cbv-index'),
     # path('redirect/<int:pk>', views.RedirectToMaktab.as_view(), name='redirect-to-maktabkhooneh'),
     path('post/', PostList.as_view(),name='post-list'),
-    path('post/<int:pk>/' , PostDetailView.as_view(), name="post-detail")
-    
+    path('post/<int:pk>/' , PostDetailView.as_view(), name="post-detail"),
+    path('post/create', PostCreateView.as_view(), name='post-create'),    
+    path('post/<int:pk>/edit/', PostEditView.as_view(), name='post-edit'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete')
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,9 +1,11 @@
 from typing import Any
 from django.db.models.query import QuerySet
+from django.forms import BaseModelForm
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
-from django.views.generic import TemplateView , RedirectView , ListView , DetailView
+from django.views.generic import *
 from .models import Post , Category
-
+from .forms import *
 # Create your views here.
 
 class IndexView(TemplateView):
@@ -25,14 +27,13 @@ class RedirectToMaktab(RedirectView):
         print(post)
         return super().get_redirect_url(*args, **kwargs)
     
-
 class PostList(ListView):
 
     model = Post
     # queryset = Post.objects.all()
     context_object_name = 'posts'
     paginate_by = 1
-    ordering = 'created_date'
+    ordering = '-created_date'
     # def get_queryset(self):
         # posts = Post.objects.filter(status = 1)
         # return posts
@@ -40,3 +41,30 @@ class PostList(ListView):
 class PostDetailView(DetailView):
     model = Post
     
+"""class PostCreateView(FormView):
+
+    template_name = 'contact.html'
+    form_class = PostForm
+    success_url = '/blog/post'
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)"""
+
+class PostCreateView(CreateView):
+    model = Post
+    # fields = ['author', 'title', 'content', 'status', 'category', 'published_date']
+    form_class = PostForm
+    success_url = '/blog/post'
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+class PostEditView(UpdateView):
+    model = Post
+    form_class = PostForm
+    success_url = '/blog/post/'
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = '/blog/post'
